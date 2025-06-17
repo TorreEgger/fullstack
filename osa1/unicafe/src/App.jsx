c499c8ed5959b500395ed18ecf681795b30e47cc
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-06-17 13:05:57 +0300

    unicafe step 2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,22 +1,42 @@
 import { useState } from 'react'
 
+
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // total samalla tavalla kuin materiaalissa, siistimpi näin
+  // kuin renderöinnin yhteydessä laskeminen
+  const [total, setTotal] = useState(0)
+  
+
+
+
+
   const handleGoodClick = () => {
+    const uusiMaara = good + 1
     setGood(good + 1)
+    setTotal(uusiMaara + neutral + bad)
   }
 
   const handleNeutralClick = () => {
+    const uusiMaara = neutral + 1
     setNeutral(neutral + 1)
+    setTotal(uusiMaara + good + bad)
   }
 
   const handleBadClick = () => {
+    const uusiMaara = bad + 1
     setBad(bad + 1)
+    setTotal(uusiMaara + good + neutral)
   }
+
+
+
 
   return (
     <div>
@@ -30,6 +50,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/total}</p>
+      <p>positive {(good/total)*100} %</p>
     </div>
   )
 }
