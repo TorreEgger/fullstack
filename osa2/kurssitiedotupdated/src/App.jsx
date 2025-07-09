6486410a4161a8e84be0ba001146ef4fc13a7309
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-07-09 12:38:36 +0300

    2.5

--- osa2/kurssitiedotupdated/src/App.jsx
+++ osa2/kurssitiedotupdated/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 const App = () => {
   const courses = [
     {
@@ -49,137 +51,11 @@ const App = () => {
    }
   ]
 
-  const kutsut = courses.map(x =>
-    <Course key={x.id}>
-      {x.parts}
-    </Course>
-  )
-  console.log('kutsut', kutsut)
-
-/*
-const kutsuFunktio = () => {
-  for(let i = 0; i<courses.length; i++) {
-    return kutsut
-  }
-}
-  */
-
 
   return (
     <div>
       <Course course={courses} />
     </div>
-  )
-}
-
-
-const Course = (props) => {
-  console.log('coursen propsit', props)
-
-
-
-    const otsikot = props.course.map(x =>
-    <h1 key={x.id}>
-      {x.name}
-    </h1>
-  )
-
-
-  console.log(otsikot)
-
-   const sisalto = props.course.map(x =>
-    <Course key={x.id}>
-      {x.parts}
-    </Course>
-  )
-
-
-  console.log(sisalto)
-
-
-  return (
-    <div>
-      <h1>Web development curriculum</h1>
-      {
-        props.course.map(x =>
-          <div key={x.id}>
-            <Header course={x.name}/> <Content parts={x.parts} /> <Total parts={x.parts} />
-          </div>
-        )
-      }
-    </div>
-  )
-}
-
-
-const Header = (props) => {
-  console.log('headerin propsit', props)
-  return (
-    <div>
-      <h2>{props.course}</h2>
-    </div>
-  )
-}
-
-
-const Content = (props) => {
-  console.log('contentin propsit', props)
-  return (
-   <div>
-    <Part parts={props.parts} />
-   </div>
-  )
-}
-
-
-const Part = (props) => {
-  console.log('partin propsit', props)
-  const sisalto = props.parts.map(x =>
-      <p key={x.id}>
-        {x.name} {x.exercises}
-      </p>
-  )
-
-
-  return (
-   <div>
-    {sisalto}
-   </div>
-  )
-}
-
-
-const Total = (props) => { 
-console.log('totalin propsit', props.parts[0].exercises)
-
-/*let sum = 0
-for(let i = 0; i<props.parts.length; i++) {
-  sum += props.parts[i].exercises
-}
-  */
-
-
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-/*const sum = props.parts.reduce((
-  accumulator, current) => {
-    console.log('accumulator', accumulator)
-    console.log('current', current.exercises)
-    return accumulator.exercises + current.exercises
-  }
-) 
-  */
-
-const sum = props.parts.reduce(
-  (accumulator, current) => accumulator + 
-  current.exercises,
-  0
-)
-
-
-
-
-  return (
-    <div><h2>total of {sum} exercises</h2></div>
   )
 }
 
---
+++ osa2/kurssitiedotupdated/src/components/Course.jsx
@@ -0,0 +1,91 @@
+const Course = ({ course }) => {
+  console.log('coursen propsit', course)
+
+    const otsikot = course.map(x =>
+    <h1 key={x.id}>
+      {x.name}
+    </h1>
+  )
+
+
+  console.log(otsikot)
+
+   const sisalto = course.map(x =>
+    <Course key={x.id}>
+      {x.parts}
+    </Course>
+  )
+
+
+  console.log(sisalto)
+
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {
+        course.map(x =>
+          <div key={x.id}>
+            <Header course={x.name}/> <Content parts={x.parts} /> <Total parts={x.parts} />
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+
+const Header = ({ course }) => {
+  console.log('headerin propsit', course)
+  return (
+    <div>
+      <h2>{course}</h2>
+    </div>
+  )
+}
+
+
+const Content = ({ parts }) => {
+  console.log('contentin propsit', parts)
+  return (
+   <div>
+    <Part parts={parts} />
+   </div>
+  )
+}
+
+
+const Part = ({ parts }) => {
+  console.log('partin propsit', parts)
+  const sisalto = parts.map(x =>
+      <p key={x.id}>
+        {x.name} {x.exercises}
+      </p>
+  )
+
+
+  return (
+   <div>
+    {sisalto}
+   </div>
+  )
+}
+
+
+const Total = ({ parts }) => { 
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+
+const sum = parts.reduce(
+  (accumulator, current) => accumulator + 
+  current.exercises,
+  0
+)
+
+  return (
+    <div><h2>total of {sum} exercises</h2></div>
+  )
+}
+
+export default Course
