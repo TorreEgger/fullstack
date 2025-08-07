d4e9da91fa277f757883fd331939c5a93b877cf4
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-08-07 20:10:36 +0300

    back to start

--- osa3/puhelinluettelo-osa3/index.js
+++ osa3/puhelinluettelo-osa3/index.js
@@ -27,7 +27,6 @@ let persons = [
     }
 ]
 
-app.use(express.static('dist'))
 app.use(express.json())
 
 
@@ -122,7 +121,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
