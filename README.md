4fc1ab7d44d357a9afcd057eec38e40331756340
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-08-07 15:11:00 +0300

    3.9 again

--- README.md
+++
@@ -1,3 +0,0 @@
-Eevert Salmisen Fullstack-tehtäviä:
-
-Puhelinluettelon verkkosivu: https://fullstack-p4d0.onrender.com
--- osa3/puhelinluettelo-osa3/index.js
+++ osa3/puhelinluettelo-osa3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 
 
 let persons = [
@@ -122,7 +124,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
