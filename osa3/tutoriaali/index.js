466aae1ca005bbae84e226afe0e2fbc3a15e6c58
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-08-11 12:08:14 +0300

    uibuild

--- osa3/tutoriaali/index.js
+++ osa3/tutoriaali/index.js
@@ -1,5 +1,13 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
+
+
 const app = express()
+
+
+
+
 
 let notes = [
   {
@@ -28,16 +36,18 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -92,7 +102,8 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
