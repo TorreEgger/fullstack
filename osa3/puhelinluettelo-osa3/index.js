a0ced796fff6122a8916d4a85c4656814ff5c5e5
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-09-19 16:03:38 +0300

    3.16

--- osa3/puhelinluettelo-osa3/index.js
+++ osa3/puhelinluettelo-osa3/index.js
@@ -46,31 +46,26 @@ app.get('/api/persons', (request, response) => {
 
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
+            console.log(person)
         } else {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id'})
-    })
+    .catch(error => next(error))
 })
 
 
 // had to watch for 3.15 how i did this previously
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id'})
-    })
+    .catch(error => next(error))
 })
     
 
@@ -82,24 +77,21 @@ const generateId = () => {
 
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     console.log(request.body)
     const body = request.body
 
 
     if (!body.name && !body.number) {
-        return response.status(400).json({
-            error: 'no information given' })
+        return next('no information given')
     }
 
     if (!body.name) {
-        return response.status(400).json({
-            error: 'name is missing' })
+        return next('name is missing')
     }
 
     if (!body.number) {
-        return response.status(400).json({
-            error: 'number is missing' })
+        return next('number is missing')
     }
 
    
@@ -107,8 +99,7 @@ app.post('/api/persons', (request, response) => {
     // Täältä katsottu apua, että miten regexillä saa tuon haun tunnistamaan merkkijonot riippumatta "casesta"
     Person.findOne({ name: { $regex: body.name, $options: 'i' }}).then(result => {
         if(result)
-            return response.status(400).json({
-                error: 'name must be unique' })
+            return next('name must be unique')
         
         const person = new Person({
         name: body.name,
@@ -127,57 +118,47 @@ app.post('/api/persons', (request, response) => {
 
 // oletattomia osoitteita varten. ei kuulu virheiden käsittelyyn
 const unknownEndpoint = (request, response) => {
-    return response.status(404).send({ error: 'unknown endpoint'})
+    return response.status(404).send({ error: 'unknown endpoint' })
 }
 
 
 app.use(unknownEndpoint)
 
-/*
+
 const errorHandler = (error, request, response, next) => {
 
-    console.log('tähän errorHandlerin tulostus', error.message)
+    if (error.name)
+        console.log(error.name)
 
 
     if (error.name === 'CastError') {
         console.log(error)
-        return response.status(400).send({ error: 'malformatted id'})
+        return response.status(400).send({ error: 'malformatted id' })
     }
 
     
     if (error === 'no information given') {
         console.log(error)
-        return response.status(400).send({ error: 'no information given'})
+        return response.status(400).send({ error: 'no information given' })
     }
 
 
     if (error === 'name is missing') {
         console.log(error)
-        return response.status(400).send({ error: 'name is missing'})
+        return response.status(400).send({ error: 'name is missing' })
     }
 
 
     if (error === 'number is missing') {
         console.log(error)
-        return response.status(400).send({ error: 'number is missing'})
+        return response.status(400).send({ error: 'number is missing' })
     }
 
 
     if (error === 'name must be unique') {
         console.log(error)
-        return response.status(400).send({ error: 'name must be unique'})
+        return response.status(400).send({ error: 'name must be unique' })
     }
-        
-
-
-    
-    if (error.message === 'unknown endpoint') {
-        console.log(error)
-        return response.status(404).send({ error: 'unknown endpoint'})
-    }
-
-    
-        
         
 
     //console.log(next(error))
@@ -189,7 +170,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-*/
+
 
 
 const PORT = process.env.PORT
