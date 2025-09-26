0626d9d07c308fd436acfe861bd4fb3354ffe13d
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-09-26 16:08:58 +0300

    uibuild

--- osa3/puhelinluettelo-osa3/index.js
+++ osa3/puhelinluettelo-osa3/index.js
@@ -17,7 +17,6 @@ app.use(morgan('tiny'))
 // tutoriaalissa oli ongelmia miten .env-tiedosto luodaan, kun visual studio pisti sen txt-tyyppiseksi. sitten katsoin täältä miten se tehdään
 // https://www.reddit.com/r/react/comments/p9a9od/how_to_create_a_env_file/
 // nyt kuitenkin siirsin sisällön uuteen tiedostoon, nimesin sen .evn ja uudelleennimesin sen file explorerissa
-
 app.get('/info', (request, response) => {
     let date = Date()
     let count = 0
