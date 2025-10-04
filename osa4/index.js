5757b775a090da15d84a59ce7e21cd873171d7b2
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-10-04 20:19:51 +0300

    logger.js again

--- osa4/index.js
+++ osa4/index.js
@@ -1,6 +1,7 @@
 require ('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 //https://www.reddit.com/r/react/comments/p9a9od/how_to_create_a_env_file/
 
@@ -34,9 +35,9 @@ mongoose.connect(mongoUrl)
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path: ', request.path)
-  console.log('Body: ', request.body)
+  logger.info('Method:', request.method)
+  logger.info('Path: ', request.path)
+  logger.info('Body: ', request.body)
   next()
 }
 
