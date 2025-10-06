9d0f111bc70deeffb71746d9cd92bb5389891b11
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-10-06 15:43:42 +0300

    went back

--- osa4/app.js
+++ osa4/app.js
@@ -1,12 +1,29 @@
-/*const express = require('express')
+const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const logger = require('/utils/logger')
-const blogsRouter = require('/controllers/blogs')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blog')
 
 
 const app = express()
 
 
 logger.info('connecting to', config.MONGODB_URI)
-*/
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+app.use(express.json())
+
+
+app.use('/api/blog', blogsRouter)
+
+
+module.exports = app
--- osa4/index.js
+++ osa4/index.js
@@ -67,7 +67,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 
 
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
