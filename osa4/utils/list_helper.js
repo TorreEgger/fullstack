753d69fb111e81deb6cd60541be9eab516500c8a
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-10-13 16:35:51 +0300

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,7 +2,26 @@ const dummy = (blogs) => {
   return 1
 }
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+//https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#node-sovellusten-testaaminen
+const totalLikes = (blogs) => {
+  const reducer = (sum, currentValue) => {
+    return sum + currentValue.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+  /*
+  let sum = 0
+
+  for(let i = 0; i<blogs.length; i++) {
+    sum += blogs[i].likes
+  }
+  return sum
+  */
+}
+
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
