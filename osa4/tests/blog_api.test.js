25236aa7861df633c3f1afe0f46e934be83d8c1c
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2026-01-17 18:10:59 +0200

    listää testejä 4.13 ja 4.14 sekä describe-lohkot

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -17,171 +17,204 @@ beforeEach(async () => {
 })
 
 
+describe('correct format-testing', () => {
 
-test('all blogs are returned in JSON-format', async () => {
-  const response = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('all blogs are returned in JSON-format', async () => {
+    const response = await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+
+  test('all blogs should contain an id correctly', async () => {
+    const response = await api.get('/api/blogs')
+
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+    const idOikein = (currentValue) => Object.hasOwn(currentValue, 'id')
+
+    //console.log(response.body.every(idOikein))
+    /*
+    let bool = false
+    for (let i = 0; i<response.body.length; i++) {
+      console.log(bool)
+      bool = Object.hasOwn(response.body[i], '_id')
+    }
+    */
+    //console.log(bool, 'bool')
+    assert.strictEqual(response.body.every(idOikein), true)
+
+  })
+
+
+
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Can anything knock China off its mountain?',
+      author: 'Noah Smith',
+      url: 'https://www.noahpinion.blog/p/can-anything-knock-china-off-its',
+      likes: 324
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+
+    //console.log(blogsAtEnd[blogsAtEnd.length-1].title, 'title')
+    //const titles = blogsAtEnd.map(x => x.title)
+    assert.strictEqual(blogsAtEnd[blogsAtEnd.length-1].title, newBlog.title)
+
+  })
 })
 
+describe('incorrect data testing in post-method', () => {
 
-test('all blogs should contain an id correctly', async () => {
-  const response = await api.get('/api/blogs')
+  test('likes will be set to zero if left empty', async () => {
 
-  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-  const idOikein = (currentValue) => Object.hasOwn(currentValue, 'id')
+    const newBlog = {
+      title: 'Happy more waking that running',
+      author: 'Jesse Guth',
+      url: 'https://reallynotarunner.com/2025/07/21/happy-more-waking-than-running/',
+      likes: null
+    }
 
-  //console.log(response.body.every(idOikein))
-  /*
-  let bool = false
-  for (let i = 0; i<response.body.length; i++) {
-    console.log(bool)
-    bool = Object.hasOwn(response.body[i], '_id')
-  }
-  */
-  //console.log(bool, 'bool')
-  assert.strictEqual(response.body.every(idOikein), true)
-
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Can anything knock China off its mountain?',
-    author: 'Noah Smith',
-    url: 'https://www.noahpinion.blog/p/can-anything-knock-china-off-its',
-    likes: 324
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blogsAtEnd = await helper.blogsInDb()
 
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(blogsAtEnd[blogsAtEnd.length-1].likes, 0)
+
+  })
 
 
-  //console.log(blogsAtEnd[blogsAtEnd.length-1].title, 'title')
-  //const titles = blogsAtEnd.map(x => x.title)
-  assert.strictEqual(blogsAtEnd[blogsAtEnd.length-1].title, newBlog.title)
+  test('blogs missing a title will equate to a bad request (400)', async () => {
+    const newBlog = {
+      author: 'Noah Smith',
+      url: 'https://www.noahpinion.blog/p/roundup-75-checking-in-on-the-bad',
+      likes: 51
+    }
 
-})
-
-
-
-test('likes will be set to zero if left empty', async () => {
-
-  const newBlog = {
-    title: 'Happy more waking that running',
-    author: 'Jesse Guth',
-    url: 'https://reallynotarunner.com/2025/07/21/happy-more-waking-than-running/',
-    likes: null
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
 
-  const blogsAtEnd = await helper.blogsInDb()
+    const blogsAtEnd = await helper.blogsInDb()
 
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 
-  assert.strictEqual(blogsAtEnd[blogsAtEnd.length-1].likes, 0)
-
-})
-
-
-test('blogs missing a title will equate to a bad request (400)', async () => {
-  const newBlog = {
-    author: 'Noah Smith',
-    url: 'https://www.noahpinion.blog/p/roundup-75-checking-in-on-the-bad',
-    likes: 51
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-
-})
+  })
 
 
 
-test('blogs missing an url will equate to a bad request (400)', async () => {
-  const newBlog = {
-    title: 'TENACITY',
-    author: 'JASON JUNG',
-    likes: 2
-  }
+  test('blogs missing an url will equate to a bad request (400)', async () => {
+    const newBlog = {
+      title: 'TENACITY',
+      author: 'JASON JUNG',
+      likes: 2
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
-  const blogsAtEnd = await helper.blogsInDb()
+    const blogsAtEnd = await helper.blogsInDb()
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 
-})
-
-
-
-// poistamisen toteutus myös samanlainen kuin kurssilla
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
-
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
-
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  const ids = blogsAtEnd.map(b => b.id)
-  assert(!ids.includes(blogToDelete.id))
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
 })
 
 
 
-test('the content of a blog can be changed', async () => {
+describe('tests regarding delete', () => {
 
-  const blogsAtDb = await helper.blogsInDb()
-  const blogToModify = blogsAtDb[0]
+  // poistamisen toteutus myös samanlainen kuin kurssilla
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-
-  blogToModify.likes = 500
-
-
-  await api
-    .put(`/api/blogs/${blogToModify.id}`)
-    .send(blogToModify)
-    .expect(200)
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
 
-  const blogsAtEnd = await helper.blogsInDb()
+    const blogsAtEnd = await helper.blogsInDb()
 
-  assert.strictEqual(500, blogsAtEnd[0].likes)
+    const ids = blogsAtEnd.map(b => b.id)
+    assert(!ids.includes(blogToDelete.id))
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
 
 
+  test('no blog will be deleted if id is wrong', async () => {
+    await api.delete('/api/blog/4').expect(404)
+  })
 
 })
 
+describe('tests regarding put-method', () => {
+
+
+  test('the content of a blog can be changed', async () => {
+
+    const blogsAtDb = await helper.blogsInDb()
+    const blogToModify = blogsAtDb[0]
+
+
+    blogToModify.likes = 500
+
+
+    await api
+      .put(`/api/blogs/${blogToModify.id}`)
+      .send(blogToModify)
+      .expect(200)
+
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(500, blogsAtEnd[0].likes)
+
+
+
+  })
+
+
+  test('the content of a blog will not be changed if the data is invalid', async() => {
+    const blogsAtDb = await helper.blogsInDb()
+    const blogToModify = blogsAtDb[0]
+
+
+    blogToModify.likes = 'mansikki'
+
+
+    await api
+      .put(`/api/blogs/${blogToModify.id}`)
+      .send(blogToModify)
+      .expect(400)
+
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert(blogsAtEnd[0].likes !== blogToModify.likes)
+  })
+})
 
 
 
