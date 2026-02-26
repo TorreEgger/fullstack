957730be38ca5fe3412ee57f0f0b18fea6689a73
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2026-02-26 17:49:09 +0200

    5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [view, setView] = useState(true)
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+
+  if (view === false) {
+
+    console.log(view, 'false branch')
+    return (
+    <div style={blogStyle}>
+       <li>{blog.title} {blog.author} <button onClick={() => setView(!view)}>hide</button></li>
+       <li>{blog.url}</li>
+       <li>{blog.likes} <button>like</button></li>
+       <li>{blog.user.name}</li>
+    </div>
+    )
+  }
+
+
+   return (
+    <div style={blogStyle}>
+      <div> 
+        {blog.title} {blog.author} <button onClick={() => setView(!view)}>view</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
