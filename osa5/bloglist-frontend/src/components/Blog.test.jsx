3359997b394e8343ce48f4e2c2787bbd9921b368
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2026-03-10 17:45:59 +0200

    5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title but not title or url', () => {
@@ -26,7 +27,7 @@ test('renders title but not title or url', () => {
     'https://blogs.com', { exact: false }
   )
 
-  console.log(element2)
+  //console.log(element2)
 
   expect(element2).toBeNull()
 
@@ -36,7 +37,62 @@ test('renders title but not title or url', () => {
   )
 
   expect(element3).toBeNull()
+})
+
+test('if view-button is pressed, every field is shown', async () => {
+  const blog = {
+    title: 'reading is rewarding',
+    author: 'Josh Fox',
+    url: 'https://readersopinion',
+    likes: 103,
+    user: {
+      username: 'keijo',
+      name: 'keke',
+      id: '6994284305971abb49d1dfdc'
+    }
+  }
+
+  const user = {
+    user: {
+      username: 'keijo',
+      name: 'keke',
+      id: '6994284305971abb49d1dfdc'
+    }
+  }
+
+  console.log(user.user.name)
+  console.log(blog.user.name)
+
+  //const mockHandler = vi.fn()
 
 
+  render(<Blog blog={blog} user={user.user} />)
+
+
+  const kayttaja = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await kayttaja.click(button)
+
+  //screen.debug()
+
+  const element = screen.getByText(
+    'https://readersopinion', { exact: false }
+  )
+
+  expect(element).toBeDefined()
+
+  const element2 = screen.getByText(
+    '103', { exact: false }
+  )
+
+  expect(element2).toBeDefined()
+
+
+  const element3 = screen.getByText(
+    'keke', { exact: false }
+  )
+
+  expect(element3).toBeDefined()
 
 })
